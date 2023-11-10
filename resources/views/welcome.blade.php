3611a58f91ca79c9f129f56c164686ccbd0c7f8e
Gregory Ross <gregross.dev@gmail.com>
2023-11-10 15:37:57 -0500

    home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
 <x-layouts.app>
     <section class="grid grid-cols-1 gap-4 justify-items-center p-16">
+        @auth
         <h2 class="mb-4 text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">
             {{ Auth::user()->name }}
         </h2>
+        @endauth
         <a href="/add-meeting" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full">
             <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://images.unsplash.com/photo-1615326636917-94b09ddefded?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Add New Meeting">
             <div class="flex flex-col justify-between p-4 leading-normal">
