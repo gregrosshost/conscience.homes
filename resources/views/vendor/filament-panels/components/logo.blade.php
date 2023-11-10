4576afdb9bb6eff19a9a656e27f5e196fc4151c0
Gregory Ross <gregross.dev@gmail.com>
2023-11-10 16:14:12 -0500

    style

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,8 +1,8 @@
 <h2 style="font-size: 24px; font-weight: bold">
+    <a class="ml-4 p-4 bg-blue-400" href="/">Home</a>
     @if (!empty(Auth::user()->name))
         {{ Auth::user()->name }}
     @else
         Conscience Homes
     @endif
-    <a class="ml-4 p-4 bg-blue-400" href="/">Home</a>
 </h2>
