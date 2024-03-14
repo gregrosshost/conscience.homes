d7cc0022889d189195746feec195d18c37d2ede8
Gregory Ross <77366132+gregrossdev@users.noreply.github.com>
2024-03-14 17:45:55 -0400

    Update create-meeting.blade.php

--- resources/views/livewire/create-meeting.blade.php
+++ resources/views/livewire/create-meeting.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <form wire:submit="create" class="mx-auto max-w-lg rounded-lg border bg-gray-50 shadow-2xl">
+    <form wire:submit="create" class="mx-auto max-w-lg rounded-lg border bg-gray-50 dark:bg-gray-800 shadow-2xl">
         <div class="flex flex-col gap-4 p-4 md:p-8">
-            <h2 class="mb-4 text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">
+            <h2 class="mb-4 text-2xl font-bold text-gray-800 dark:text-gray-50 md:mb-8 lg:text-3xl">
                 {{ Auth::user()->name }}
             </h2>
 
